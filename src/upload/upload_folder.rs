cebf8a21434a79b8e0c7970d7739e874e4e0400a
Philpax <me@philpax.me>
2026-01-31 00:42:44 +0100

    fix(upload): save initial upload progress

--- src/upload/upload_folder.rs
+++ src/upload/upload_folder.rs
@@ -217,7 +217,12 @@ pub async fn upload_folder(
             init_response.expires_at,
         );
 
-        LocalRecordingPaused::new(info, metadata, upload_progress)
+        let paused = LocalRecordingPaused::new(info, metadata, upload_progress);
+        // Ensure we save initial progress so that the chunk-completion-recorder has something to work with
+        paused
+            .save_upload_progress()
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        paused
     };
 
     Ok(super::upload_tar::run(
